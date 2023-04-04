b9d6da39836344c7a8764dc8f5e14156c11a741c
Volodymyr Donets <volodons@gmail.com>
2023-04-04 19:34:08 +0300

    Add button interactivity

---
+++ task2_2/scripts/script.js
@@ -0,0 +1,12 @@
+"use strict";
+const buttonAll = document.getElementById("all");
+const buttonBreakfast = document.getElementById("breakfast");
+const buttonLunch = document.getElementById("lunch");
+const buttonDinner = document.getElementById("dinner");
+const buttonDrinks = document.getElementById("drinks");
+
+buttonAll.addEventListener("click", function);
+buttonBreakfast.addEventListener("click", function);
+buttonLunch.addEventListener("click", function);
+buttonDinner.addEventListener("click", function);
+buttonDrinks.addEventListener("click", function);
