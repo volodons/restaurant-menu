c5d762378cc5fe7e46659089eecf991af33bf58e
Volodymyr Donets <volodons@gmail.com>
2023-04-04 22:39:24 +0300

    Add items data to data.js

---
+++ task2_2/scripts/data.js
@@ -0,0 +1,72 @@
+export const data = [
+  {
+    name: 'Pasta';
+    price: '$15.99';
+    description: "I'm baby woke milkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold- pressed";
+    image: './images/dinner-pasta.webp';
+    category: 'Dinner';
+  },
+  {
+    name: 'Bacon with sausages and eggs';
+    price: '$20.99';
+    description: 'Vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats';
+    image: './images/breakfast-bacon-sausages-eggs.jpg';
+    category: 'Breakfast';
+  },
+  {
+    name: 'Pork';
+    price: '$35.99';
+    description: 'Ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral';
+    image: './images/dinner-pork.jpeg';
+    category: 'Dinner';
+  },
+  {
+    name: 'Americano';
+    price: '$5.99';
+    description: 'Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism ausstin mlkshk truffaut';
+    image: './images/drink-americano.jpeg';
+    category: 'Drinks';
+  },
+  {
+    name: 'Sushi';
+    price: '$28.99';
+    description: "Franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up";
+    image: './images/dinner-sushi.jfif';
+    category: 'Dinner';
+  },
+  {
+    name: 'Latte';
+    price: '$8.99';
+    description: 'Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday';
+    image: './images/drink-latte.jpeg';
+    category: 'Drinks';
+  },
+  {
+    name: 'Bacon with salad';
+    price: '$16.99';
+    description: 'Carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird';
+    image: './images/breakfast-bacon-salad.jpeg';
+    category: 'Breakfast';
+  },
+  {
+    name: 'Salad';
+    price: '$12.99';
+    description: 'On it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut';
+    image: './images/lunch-salad.jpeg';
+    category: 'Lunch';
+  },
+  {
+    name: 'Tea';
+    price: '$6.99';
+    description: 'Skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing';
+    image: './images/drink-tea.jpg';
+    category: 'Drinks';
+  },
+  {
+    name: 'Hamburger with fries';
+    price: '$19.99';
+    description: 'Skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing';
+    image: './images/breakfast-hamburger-fries.webp';
+    category: 'Breakfast';
+  },
+]
