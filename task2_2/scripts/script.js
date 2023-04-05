ab111f9479370e391e4d6c8a00978cb072f353b0
Volodymyr Donets <volodons@gmail.com>
2023-04-05 11:30:47 +0300

    Add code to import data from another JS file

--- task2_2/scripts/script.js
+++ task2_2/scripts/script.js
@@ -1,17 +1,24 @@
 "use strict";
-// import { dishes } from "data.js";
+
+import { dishes } from "./data.js";
 const buttonAll = document.getElementById("all");
 const buttonBreakfast = document.getElementById("breakfast");
 const buttonLunch = document.getElementById("lunch");
 const buttonDinner = document.getElementById("dinner");
 const buttonDrinks = document.getElementById("drinks");
 
-buttonAll.addEventListener("click", testFunction);
-buttonBreakfast.addEventListener("click", testFunction);
-buttonLunch.addEventListener("click", testFunction);
-buttonDinner.addEventListener("click", testFunction);
-buttonDrinks.addEventListener("click", testFunction);
+const card = document.getElementById("card");
 
-function testFunction() {
+buttonAll.addEventListener("click", allListed);
+buttonBreakfast.addEventListener("click", breakfastListed);
+buttonLunch.addEventListener("click", allListed);
+buttonDinner.addEventListener("click", allListed);
+buttonDrinks.addEventListener("click", allListed);
+
+function allListed() {
   alert("We have all dishes listed, enjoy!");
+}
+
+function breakfastListed() {
+  card.innerHTML = dishes;
 }
