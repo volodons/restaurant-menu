e55f6f35d65c541918429f2badb405d70bedf0e9
Volodymyr Donets <volodons@gmail.com>
2023-04-05 10:53:06 +0300

    Add EventListeners to buttons and Function test

--- task2_2/scripts/data.js
+++ task2_2/scripts/data.js
@@ -1,4 +1,4 @@
-export const data = [
+export const dishes = [
   {
     name: 'Pasta';
     price: '$15.99';
--- task2_2/scripts/script.js
+++ task2_2/scripts/script.js
@@ -1,12 +1,17 @@
 "use strict";
+// import { dishes } from "data.js";
 const buttonAll = document.getElementById("all");
 const buttonBreakfast = document.getElementById("breakfast");
 const buttonLunch = document.getElementById("lunch");
 const buttonDinner = document.getElementById("dinner");
 const buttonDrinks = document.getElementById("drinks");
 
-buttonAll.addEventListener("click", function);
-buttonBreakfast.addEventListener("click", function);
-buttonLunch.addEventListener("click", function);
-buttonDinner.addEventListener("click", function);
-buttonDrinks.addEventListener("click", function);
+buttonAll.addEventListener("click", testFunction);
+buttonBreakfast.addEventListener("click", testFunction);
+buttonLunch.addEventListener("click", testFunction);
+buttonDinner.addEventListener("click", testFunction);
+buttonDrinks.addEventListener("click", testFunction);
+
+function testFunction() {
+  alert("We have all dishes listed, enjoy!");
+}
