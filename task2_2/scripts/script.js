b25f5b5adae0038321b1f0d951fe8379c8ddaf10
Volodymyr Donets <volodons@gmail.com>
2023-04-09 14:43:52 +0300

    Add JS to show Dinner items

--- task2_2/scripts/script.js
+++ task2_2/scripts/script.js
@@ -15,7 +15,7 @@ const mainContent = document.querySelector(".main-content");
 buttonAll.addEventListener("click", allListed);
 buttonBreakfast.addEventListener("click", showBreakfast);
 buttonLunch.addEventListener("click", showLunch);
-buttonDinner.addEventListener("click", allListed);
+buttonDinner.addEventListener("click", showDinner);
 buttonDrinks.addEventListener("click", allListed);
 
 function allListed() {
@@ -52,6 +52,32 @@ function showLunch() {
   mainContent.innerHTML = "";
   const result = dishes.filter((dish) => {
     return dish.category === "Lunch";
+  });
+  for (let i = 0; i < result.length; i++) {
+    const html = document.createElement("div");
+    html.innerHTML = `<div class="card" id="card">
+        <img
+          class="card__image"
+          src=${result[i].image}
+          alt=${result[i].name}
+          title=${result[i].name}
+        />
+        <h2 class="card__header">
+          <span class="card__header--header-color">${result[i].name}</span>
+          <span class="card__header--decoration-color">${result[i].price}</span>
+        </h2>
+        <h3 class="card__description">
+          ${result[i].description}
+        </h3>
+      </div>`;
+    mainContent.append(html);
+  }
+}
+
+function showDinner() {
+  mainContent.innerHTML = "";
+  const result = dishes.filter((dish) => {
+    return dish.category === "Dinner";
   });
   for (let i = 0; i < result.length; i++) {
     const html = document.createElement("div");
