a64d1a2023ad834402be4e4a7248bcbb8627f0cf
Volodymyr Donets <volodons@gmail.com>
2023-04-18 23:41:03 +0300

    Refactor code: make JS functions less verbose

--- task2_2/scripts/script.js
+++ task2_2/scripts/script.js
@@ -18,52 +18,32 @@ buttonDrinks.addEventListener("click", () => show("Drinks"));
 show();
 
 function show(type) {
-  if (!type) {
-    allCards.innerHTML = "";
-    for (let i = 0; i < dishes.length; i++) {
-      const html = document.createElement("div");
-      html.innerHTML = `<div class="card" id="card">
-        <img
-          class="card__image"
-          src=${dishes[i].image}
-          alt=${dishes[i].name}
-          title=${dishes[i].name}
-        />
-        <h2 class="card__header">
-          <span class="card__header--header-color">${dishes[i].name}</span>
-          <span class="card__header--decoration-color">${dishes[i].price}</span>
-        </h2>
-        <h3 class="card__description">
-          ${dishes[i].description}
-        </h3>
-      </div>`;
-      allCards.append(html);
-    }
-    return;
-  } else {
-    allCards.innerHTML = "";
-    const filteredDishes = dishes.filter((dish) => {
-      return dish.category === type;
-    });
-    for (let i = 0; i < filteredDishes.length; i++) {
-      const html = document.createElement("div");
-      html.innerHTML = `<div class="card" id="card">
-        <img
-          class="card__image"
-          src=${filteredDishes[i].image}
-          alt=${filteredDishes[i].name}
-          title=${filteredDishes[i].name}
-        />
-        <h2 class="card__header">
-          <span class="card__header--header-color">${filteredDishes[i].name}</span>
-          <span class="card__header--decoration-color">${filteredDishes[i].price}</span>
-        </h2>
-        <h3 class="card__description">
-          ${filteredDishes[i].description}
-        </h3>
-      </div>`;
-      allCards.append(html);
-    }
-    return;
-  }
+  const currentDishes = type
+    ? dishes.filter((dish) => dish.category === type)
+    : dishes;
+
+  allCards.innerHTML = generateManyCards(currentDishes);
+}
+
+function generateManyCards(dishes) {
+  return dishes.map((dish) => generateCardHtml(dish)).join("");
+}
+
+function generateCardHtml(dish) {
+  return `
+    <div class="card" id="card">
+      <img
+        class="card__image"
+        src=${dish.image}
+        alt=${dish.name}
+        title=${dish.name}
+      />
+      <h2 class="card__header">
+        <span class="card__header--header-color">${dish.name}</span>
+        <span class="card__header--decoration-color">${dish.price}</span>
+      </h2>
+      <h3 class="card__description">
+        ${dish.description}
+      </h3>
+    </div>`;
 }
